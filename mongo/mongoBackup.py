5b9d41fb21a81224e39492d36cd2f40d1de68d46
sandip <k.sandip96@gmail.com>
2015-09-18 11:00:33 +0530

    Add Complete & Collection Wise Backup Method

--- mongo/mongoBackup.py
+++ mongo/mongoBackup.py
@@ -4,11 +4,6 @@ import os
 from pymongo import MongoClient
 import socket
 
-def getCollectionNames(dbName):
-    client = MongoClient()
-    collectionNames = client[dbName].collection_names()
-    return collectionNames
-
 def getCompleteDatabaseBackup(dbName, backupTime):
     databaseName = dbName
     outputDirectory = "/opt/mongodump-"+backupTime
@@ -16,12 +11,19 @@ def getCompleteDatabaseBackup(dbName, backupTime):
     os.system(commandToTakeCompleteDatabaseBackup)
     return outputDirectory
 
+def getCollectionWiseBackup(dbName, collectionName):
+    databaseName = dbName
+    commandToGetCollectionWiseBackup = "mongodump  --db "+databaseName+" --collection "+collectionName
+    os.system(commandToGetCollectionWiseBackup)
+    outputDirectory = "dump/"
+    return outputDirectory
+    
 def getNodeBackupS3Path(bucketName):
     nodeName = (socket.gethostname())
     nodeS3Path = "s3://"+bucketName+"/"+nodeName
     return nodeS3Path
 
-def uploadMongoCollectionsToS3(dbName, bucketName, backupTime):
+def uploadMongoCompleteBackupToS3(dbName, bucketName, backupTime):
     getCompleteDatabaseBackupDirectory = getCompleteDatabaseBackup(dbName, backupTime)
     s3SyncDir = getNodeBackupS3Path(bucketName)
     s3SyncCommand = "aws s3 sync "+getCompleteDatabaseBackupDirectory+ " "+s3SyncDir+ "/mongoBackup/" + backupTime+"/"
@@ -29,10 +31,23 @@ def uploadMongoCollectionsToS3(dbName, bucketName, backupTime):
     print s3SyncCommand
     os.system(s3SyncCommand)
 
+def uploadMongoCollectionWiseBackupToS3(dbName, collectionName, bucketName, backupTime):
+    getCollectionWiseBackupDirectory = getCollectionWiseBackup(dbName, collectionName)
+    s3SyncDir = getNodeBackupS3Path(bucketName)
+    s3SyncCommand = "aws s3 sync "+getCollectionWiseBackupDirectory+ " "+s3SyncDir+ "/mongoBackup/" + backupTime+"/"
+    os.system(s3SyncCommand)
+
+def uploadMongoCollectionWiseBackup(bucketName,dbName, backupTime):
+    s3SyncDir = getNodeBackupS3Path(bucketName)
+    s3SyncCommand = "aws s3 sync dump/"+dbName+ "/ "+s3SyncDir+ "/mongoBackup/" + backupTime+"/"
+    print "Uploading Mongodb Collection Wise Backup : <Local-2-S3>"
+    print s3SyncCommand
+    os.system(s3SyncCommand)
+
 def validateDatabase(dbName):
     client = MongoClient()
     databases = client.database_names()
-    if any(dbName in s for s in databases):
+    if dbName in databases:
         print "*******************"
     else:
         print "Please Provide a Valid Database Name"
@@ -41,9 +56,20 @@ def main():
     dbName = sys.argv[1]
     bucketName = sys.argv[2]
     backupTime = datetime.datetime.strftime(datetime.datetime.now(), '%Y%m%d%H%M%S')
-    validateDatabase(dbName)
-    uploadMongoCollectionsToS3(dbName, bucketName, backupTime)
-
+    optionToChooseCompleteOrCollectionWiseBackup = sys.argv[3]
+    collectionName = sys.argv[4]
+    
+    if optionToChooseCompleteOrCollectionWiseBackup == 'db':
+        validateDatabase(dbName)
+        uploadMongoCompleteBackupToS3(dbName, bucketName, backupTime)
+    else:
+        if optionToChooseCompleteOrCollectionWiseBackup == 'collection':
+            validateDatabase(dbName)
+            uploadMongoCollectionWiseBackupToS3(dbName, collectionName, bucketName, backupTime)
+        else:
+            print "Please Provide a Valid Option to Perform: i.e db|collection"
+            
 main()
+
 
 
