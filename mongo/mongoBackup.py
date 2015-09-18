c288b160e8f58f8cdc9eb254c754f589f3dd808e
sandip <k.sandip96@gmail.com>
2015-09-18 15:05:03 +0530

    Add logger & write logs to /var/log/mongoBackup.log

--- mongo/mongoBackup.py
+++ mongo/mongoBackup.py
@@ -3,18 +3,23 @@ import datetime
 import os
 from pymongo import MongoClient
 import socket
+import logging, logging.handlers
 
 
 def getCompleteDatabaseBackup(dbName, backupTime):
     databaseName = dbName
     outputDirectory = "/opt/mongodump-"+backupTime
     commandToTakeCompleteDatabaseBackup = "mongodump --db "+databaseName+" --out "+outputDirectory
+    logfun = logging.getLogger("logfun")
+    logfun.debug("Creating Mongodb Complete Backup ")
     os.system(commandToTakeCompleteDatabaseBackup)
     return outputDirectory
 
 def getCollectionWiseBackup(dbName, collectionName):
     databaseName = dbName
     commandToGetCollectionWiseBackup = "mongodump  --db "+databaseName+" --collection "+collectionName
+    logfun = logging.getLogger("logfun")
+    logfun.debug("Creating Mongodb Collection Backup ")
     os.system(commandToGetCollectionWiseBackup)
     outputDirectory = "dump/"
     return outputDirectory
@@ -28,14 +33,17 @@ def uploadMongoCompleteBackupToS3(dbName, bucketName, backupTime):
     getCompleteDatabaseBackupDirectory = getCompleteDatabaseBackup(dbName, backupTime)
     s3SyncDir = getNodeBackupS3Path(bucketName)
     s3SyncCommand = "aws s3 sync "+getCompleteDatabaseBackupDirectory+ " "+s3SyncDir+ "/mongoBackup/" + backupTime+"/"
-    print "Uploading Mongodb Complete Backup : <Local-2-S3>"
-    print s3SyncCommand
+    logfun = logging.getLogger("logfun")
+    logfun.debug("Uploading Mongodb Complete Backup : <Local-2-S3>")
+    logfun.debug(s3SyncCommand)
     os.system(s3SyncCommand)
 
 def uploadMongoCollectionWiseBackupToS3(dbName, collectionName, bucketName, backupTime):
     getCollectionWiseBackupDirectory = getCollectionWiseBackup(dbName, collectionName)
     s3SyncDir = getNodeBackupS3Path(bucketName)
     s3SyncCommand = "aws s3 sync "+getCollectionWiseBackupDirectory+ " "+s3SyncDir+ "/mongoBackup/" + backupTime+"/"
+    logfun = logging.getLogger("logfun")
+    logfun.debug("Uploading Mongodb Collection Backup To S3 Bucket")
     os.system(s3SyncCommand)
 
 def validateDatabase(dbName):
@@ -44,8 +52,20 @@ def validateDatabase(dbName):
     if dbName in databases:
         return True
     else:
-        print "Please Provide a Valid Database Name"
+        logfun = logging.getLogger("logfun")
+        logfun.exception("Please Provide a Valid Database Name")
         return False
+
+def validateDatabaseCollectionName(dbName, collectionName):
+    client = MongoClient()
+    collectionNames = client[dbName].collection_names()
+    if collectionName in collectionNames:
+        return True
+    else:
+        logfun = logging.getLogger("logfun")
+        logfun.exception("Please Provide a Valid Collection Name")
+        return False        
+
 
 def main():
     dbName = sys.argv[1]
@@ -53,22 +73,29 @@ def main():
     backupTime = datetime.datetime.strftime(datetime.datetime.now(), '%Y%m%d%H%M%S')
     optionToChooseCompleteOrCollectionWiseBackup = sys.argv[3]
     
-    old_stdout = sys.stdout
-    log_file = open("/var/log/mongoBackup.log","w")
-    sys.stdout = log_file    
-    
     if optionToChooseCompleteOrCollectionWiseBackup == 'db':
         if validateDatabase(dbName): 
             uploadMongoCompleteBackupToS3(dbName, bucketName, backupTime)
     else:
         if optionToChooseCompleteOrCollectionWiseBackup == 'collection':
             if validateDatabase(dbName):
-                uploadMongoCollectionWiseBackupToS3(dbName, sys.argv[4], bucketName, backupTime)
+                collectionName = sys.argv[4]
+                if validateDatabaseCollectionName(dbName, collectionName):
+                    uploadMongoCollectionWiseBackupToS3(dbName, collectionName, bucketName, backupTime)
         else:
-            print "Please Provide a Valid Option to Perform: i.e db|collection"
+            logfun = logging.getLogger("logfun")
+            logfun.exception("Please Provide a Valid Option to Perform: i.e db|collection")
+            
+# Make a global logging object.
+x = logging.getLogger("logfun")
+x.setLevel(logging.DEBUG)
     
-    sys.stdout = old_stdout
-    log_file.close()
+# This handler writes everything to a file.
+h1 = logging.FileHandler("/var/log/mongoBackup.log")
+f = logging.Formatter("%(levelname)s %(asctime)s %(funcName)s %(lineno)d %(message)s")
+h1.setFormatter(f)
+h1.setLevel(logging.DEBUG)
+x.addHandler(h1)    
                
 main()
 
