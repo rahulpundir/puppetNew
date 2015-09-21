9d24fd052f59ac13e67c77375164f5001470900d
sandip <k.sandip96@gmail.com>
2015-09-21 16:47:03 +0530

    Add backupType for listing Database

--- mongo/listS3Directory.py
+++ mongo/listS3Directory.py
@@ -6,14 +6,16 @@ def getS3Path(bucketName, S3SubDirectory):
     s3Path = "s3://"+bucketName+"/" + S3SubDirectory
     return s3Path
 
-def listS3Directory(bucketName, S3SubDirectory):
+def listS3Directory(bucketName, S3SubDirectory, backupType):
     s3Directory = getS3Path(bucketName, S3SubDirectory)
-    listCommand = "aws s3 ls "+s3Directory+"/"
+    listCommand = "aws s3 ls "+s3Directory+"/"+backupType+"/"
+    print listCommand
     os.system(listCommand)
 
 def main():
     bucketName = sys.argv[1]
     S3SubDirectory = sys.argv[2]
-    listS3Directory(bucketName, S3SubDirectory)
+    backupType = sys.argv[3]
+    listS3Directory(bucketName, S3SubDirectory, backupType)
 
 main()
