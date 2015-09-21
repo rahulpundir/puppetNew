46eae2642a629fc82c8e7b2ab073eb5ff72efc68
sandip <k.sandip96@gmail.com>
2015-09-21 12:58:14 +0530

    Add restore mongo script

--- mongo/mongoRestoreScript.py
+++ mongo/mongoRestoreScript.py
@@ -1,65 +1,62 @@
 #!/usr/bin/python
 import sys
-import socket
 import os
-from os import listdir
-from os.path import isfile, join
 from pymongo import MongoClient
+import tarfile
+import logging
 
-
-def getS3Path(bucketName, filePath):
-    s3Path = "s3://"+bucketName+"/" + filePath
+def getS3Path(bucketName, S3SubDirectory):
+    s3Path = "s3://"+bucketName+"/" + S3SubDirectory
     return s3Path
 
 
-def downloadDumpFileFromS3(bucketName, dumpFile):
-    dumpFile = dumpFile + ".tar.gz"
-    dumpS3Path = getS3Path(bucketName, dumpFile)
-    downloadMongoDumpCommand = "aws s3 cp " +  dumpS3Path + " /tmp/" + dumpFile
-    os.system(downloadMongoDumpCommand)
-#    print downloadMongoDumpCommand
+def downloadDumpFileFromS3(bucketName, S3SubDirectory, restoreFileName):
+    S3Path = getS3Path(bucketName, S3SubDirectory)
+    S3FileLocation = S3Path+ "/"+restoreFileName
+    downloadS3File = "aws s3 cp " +S3FileLocation+ " /tmp/" 
+    print downloadS3File
+    os.system(downloadS3File)
+    
 
-def extractDumpFile(dumpFile):
-    compressedDumpFile = "/tmp/" + dumpFile + ".tar.gz"
+def extractDumpFile(database, restoreFileName):
+    compressedDumpFileLocation = "/tmp/" + restoreFileName
+    sourceFile = tarfile.open(compressedDumpFileLocation)
+    sourceFile.extractall()
+    sourceFile.close()
 
-    extractCommand = "rm -rf /tmp/mongorestore;mkdir /tmp/mongorestore; tar -xvzf " + compressedDumpFile + " -C /tmp/mongorestore; rm -f " + compressedDumpFile
-    os.system(extractCommand)
-#    print extractCommand
-
-def restoreDumpInMongoDB(dumpFIle):
-    dumpRestoreCommand = "mongorestore /tmp/mongorestore/" + dumpFile
+def restoreCompleteDumpInMongoDB(databse):
+    dumpRestoreCommand = "mongorestore " + databse
     os.system(dumpRestoreCommand)
-#    print dumpRestoreCommand
 
-def cleanDatabaseBeforeImportCollections():
+def dropDatabase(database):
     mongodatabaseInstanceConnection = MongoClient()
-    databaseName = dbName
-    for collection in getCollectionList():
-        collectionName = collection.split('.')[0]
-        mongodatabaseInstanceConnection[databaseName].drop_collection(collectionName)   
-
-def validateDatabase():
+    mongodatabaseInstanceConnection.drop_database(database)
+    
+def validateDatabase(database):
     client = MongoClient()
     databases = client.database_names()
-    if any(dbName in s for s in databases):
-        print "*************"
+    if database in databases:
+        return True
     else:
+        logging.exception("Provided Database "+ database+ " Not exists...")
         print "Please Provide a Valid Database Name"
+        return False
     
 def main():
-    if optionToCleanUPCollectionBeforeRestore == 'restoreDatabaseWithCleanUP':
-        validateDatabase()
-#        cleanDatabaseBeforeImportCollections()
-        downloadDumpFileFromS3(bucketName, dumpFile)
-	extractDumpFile(dumpFile)
-        restoreDumpInMongoDB(dumpFile)
-    else:
-        print "Please Select Database Clean-up before Restore Operation"
-        
-dbName = sys.argv[1]
-restorePoint = sys.argv[2]
-bucketName = sys.argv[3] 
-optionToCleanUPCollectionBeforeRestore = sys.argv[4]
-dumpFile = sys.argv[5]
+    database = sys.argv[1]
+    restoreFileName = sys.argv[2]
+    bucketName = sys.argv[3] 
+    S3SubDirectory = sys.argv[4]
+    optionToCleanUPCollectionBeforeRestore = sys.argv[5]
+    
+    if validateDatabase(database):
+        if optionToCleanUPCollectionBeforeRestore == 'restoreDatabaseWithCleanUP':
+            downloadDumpFileFromS3(bucketName, S3SubDirectory, restoreFileName)
+            extractDumpFile(database, restoreFileName)
+            dropDatabase(database)
+            restoreCompleteDumpInMongoDB(database)
+            
+        else:
+            print "Please Select Database Clean-up before Restore Operation"
 
 main()
