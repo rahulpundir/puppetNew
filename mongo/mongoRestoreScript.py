8805133c15968b7a0ad4efb4fb1757f0b388db62
sandip <k.sandip96@gmail.com>
2015-09-21 14:56:04 +0530

    Add collection restore option

--- mongo/mongoRestoreScript.py
+++ mongo/mongoRestoreScript.py
@@ -5,16 +5,15 @@ from pymongo import MongoClient
 import tarfile
 import logging
 
-def getS3Path(bucketName, S3SubDirectory):
-    s3Path = "s3://"+bucketName+"/" + S3SubDirectory
+def getS3Path(s3Directory):
+    s3Path = "s3://"+s3Directory
     return s3Path
 
 
-def downloadDumpFileFromS3(bucketName, S3SubDirectory, restoreFileName):
-    S3Path = getS3Path(bucketName, S3SubDirectory)
+def downloadDumpFileFromS3(s3Directory, restoreFileName):
+    S3Path = getS3Path(s3Directory)
     S3FileLocation = S3Path+ "/"+restoreFileName
     downloadS3File = "aws s3 cp " +S3FileLocation+ " /tmp/" 
-    print downloadS3File
     os.system(downloadS3File)
     
 
@@ -24,13 +23,18 @@ def extractDumpFile(database, restoreFileName):
     sourceFile.extractall()
     sourceFile.close()
 
-def restoreCompleteDumpInMongoDB(databse):
-    dumpRestoreCommand = "mongorestore " + databse
+def restoreDumpInMongoDB(database):
+    dumpRestoreCommand = "mongorestore " + database
     os.system(dumpRestoreCommand)
 
 def dropDatabase(database):
     mongodatabaseInstanceConnection = MongoClient()
     mongodatabaseInstanceConnection.drop_database(database)
+    
+def dropCollection(database, collection):
+    mongodatabaseInstanceConnection = MongoClient()
+    mongodatabaseInstanceConnection[database].drop_collection(collection)
+    
     
 def validateDatabase(database):
     client = MongoClient()
@@ -41,22 +45,67 @@ def validateDatabase(database):
         logging.exception("Provided Database "+ database+ " Not exists...")
         print "Please Provide a Valid Database Name"
         return False
+
+def validateDatabaseCollectionName(database, collection):
+    client = MongoClient()
+    collectionNames = client[database].collection_names()
+    if collection in collectionNames:
+        return True
+    else:
+        logging.exception("Provided Collection "+ collection +" not exists in Database "+database )
+        print "Please Provide a Valid Collection Name"
+        return False
+
+def restoreMongoDatabase(databaseCleanupOption, s3Directory, restoreFileName, database):
+    if databaseCleanupOption == 'withcleanup':
+        downloadDumpFileFromS3(s3Directory, restoreFileName)
+        extractDumpFile(database, restoreFileName)
+        dropDatabase(database)
+        restoreDumpInMongoDB(database)
+    else:
+        if databaseCleanupOption == 'withoutcleanup':
+            downloadDumpFileFromS3(s3Directory, restoreFileName)
+            extractDumpFile(database, restoreFileName)
+            restoreDumpInMongoDB(database)
+        else:
+            print "Please Select Database Clean-up option before Restore Operation i.e withcleanup|withoutcleanup"     
+    
+def restoreMongoCollection(collection, databaseCleanupOption, s3Directory, restoreFileName, database):
+    if databaseCleanupOption == 'withcleanup':
+        downloadDumpFileFromS3(s3Directory, restoreFileName)
+        extractDumpFile(database, restoreFileName)
+        dropCollection(database, collection)
+        restoreDumpInMongoDB(database)
+    else:
+        if databaseCleanupOption == 'withoutcleanup':
+            downloadDumpFileFromS3(s3Directory, restoreFileName)
+            extractDumpFile(database, restoreFileName)
+            restoreDumpInMongoDB(database)
+        else:
+            print "Please Select Database Clean-up option before Restore Operation i.e withcleanup|withoutcleanup"
+    
     
 def main():
     database = sys.argv[1]
     restoreFileName = sys.argv[2]
-    bucketName = sys.argv[3] 
-    S3SubDirectory = sys.argv[4]
-    optionToCleanUPCollectionBeforeRestore = sys.argv[5]
+    s3Directory = sys.argv[3]
+    restoreType = sys.argv[4]
+    databaseCleanupOption = sys.argv[5]
     
     if validateDatabase(database):
-        if optionToCleanUPCollectionBeforeRestore == 'restoreDatabaseWithCleanUP':
-            downloadDumpFileFromS3(bucketName, S3SubDirectory, restoreFileName)
-            extractDumpFile(database, restoreFileName)
-            dropDatabase(database)
-            restoreCompleteDumpInMongoDB(database)
-            
+        if restoreType == 'database':
+            restoreMongoDatabase(databaseCleanupOption, s3Directory, restoreFileName, database)
         else:
-            print "Please Select Database Clean-up before Restore Operation"
+            if restoreType == 'collection':
+                collection = sys.argv[6]
+                if validateDatabaseCollectionName(database, collection):
+                    restoreMongoCollection(collection, databaseCleanupOption, s3Directory, restoreFileName, database) 
+
 
 main()
+
+
+
+
+
+
