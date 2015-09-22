9ca60d98e808e68fa2275c45453e862936eac19f
sandip <k.sandip96@gmail.com>
2015-09-22 18:30:08 +0530

    Add mongo connection using --host option

--- mongo/mongoRestoreScript.py
+++ mongo/mongoRestoreScript.py
@@ -25,13 +25,13 @@ def extractDumpFile(database, restoreFileName):
     sourceFile.extractall()
     sourceFile.close()
 
-def restoreDumpInMongoDB(database):
-    dumpRestoreCommand = "mongorestore " + database
+def restoreDumpInMongoDB(database, getInternalIP):
+    dumpRestoreCommand = "mongorestore --host " + getInternalIP + " " + database
     logging.debug("Restoring Mongo Backup on Database " +database )
     os.system(dumpRestoreCommand)
 
-def dropDatabase(database):
-    mongodatabaseInstanceConnection = MongoClient()
+def dropDatabase(database, getInternalIP):
+    mongodatabaseInstanceConnection = MongoClient(getInternalIP)
     logging.debug("Droping Mongo Database " +database )
     mongodatabaseInstanceConnection.drop_database(database)
     
@@ -41,8 +41,8 @@ def dropCollection(database, collection):
     mongodatabaseInstanceConnection[database].drop_collection(collection)
     
     
-def validateDatabase(database):
-    client = MongoClient()
+def validateDatabase(database, getInternalIP):
+    client = MongoClient(getInternalIP)
     databases = client.database_names()
     if database in databases:
         return True
@@ -51,8 +51,8 @@ def validateDatabase(database):
         print "Please Provide a Valid Database Name"
         return False
 
-def validateDatabaseCollectionName(database, collection):
-    client = MongoClient()
+def validateDatabaseCollectionName(database, collection, getInternalIP):
+    client = MongoClient(getInternalIP)
     collectionNames = client[database].collection_names()
     if collection in collectionNames:
         return True
@@ -61,31 +61,31 @@ def validateDatabaseCollectionName(database, collection):
         print "Please Provide a Valid Collection Name"
         return False
 
-def restoreMongoDatabase(databaseCleanupOption, bucketName, nodeName, restoreFileName, database, restoreType):
+def restoreMongoDatabase(databaseCleanupOption, bucketName, nodeName, restoreFileName, database, restoreType, getInternalIP):
     if databaseCleanupOption == 'withcleanup':
         downloadDumpFileFromS3(bucketName, nodeName, restoreFileName, restoreType)
         extractDumpFile(database, restoreFileName)
-        dropDatabase(database)
-        restoreDumpInMongoDB(database)
+        dropDatabase(database, getInternalIP)
+        restoreDumpInMongoDB(database, getInternalIP)
     else:
         if databaseCleanupOption == 'withoutcleanup':
             downloadDumpFileFromS3(bucketName, nodeName, restoreFileName, restoreType)
             extractDumpFile(database, restoreFileName)
-            restoreDumpInMongoDB(database)
+            restoreDumpInMongoDB(database, getInternalIP)
         else:
             print "Please Select Database Clean-up option before Restore Operation i.e withcleanup|withoutcleanup"     
     
-def restoreMongoCollection(collection, databaseCleanupOption, bucketName, nodeName, restoreFileName, database, restoreType):
+def restoreMongoCollection(collection, databaseCleanupOption, bucketName, nodeName, restoreFileName, database, restoreType, getInternalIP):
     if databaseCleanupOption == 'withcleanup':
         downloadDumpFileFromS3(bucketName, nodeName, restoreFileName, restoreType)
         extractDumpFile(database, restoreFileName)
-        dropCollection(database, collection)
-        restoreDumpInMongoDB(database)
+        dropCollection(database, collection, getInternalIP)
+        restoreDumpInMongoDB(database, getInternalIP)
     else:
         if databaseCleanupOption == 'withoutcleanup':
             downloadDumpFileFromS3(bucketName, nodeName, restoreFileName, restoreType)
             extractDumpFile(database, restoreFileName)
-            restoreDumpInMongoDB(database)
+            restoreDumpInMongoDB(database, getInternalIP)
         else:
             print "Please Select Database Clean-up option before Restore Operation i.e withcleanup|withoutcleanup"
     
@@ -98,16 +98,19 @@ def main():
     restoreType = sys.argv[5]
     databaseCleanupOption = sys.argv[6]
     
+    getInternalIPCommand = "ifconfig eth0 | grep 'inet addr' | awk -F: '{print $2}' | awk '{print $1}'"
+    getInternalIP = os.popen(getInternalIPCommand).read().strip()
+    
     logging.basicConfig(filename='/var/log/mongoRestore.log',level=logging.DEBUG)
     
-    if validateDatabase(database):
+    if validateDatabase(database, getInternalIP):
         if restoreType == 'database':
-            restoreMongoDatabase(databaseCleanupOption, bucketName, nodeName, restoreFileName, database, restoreType)
+            restoreMongoDatabase(databaseCleanupOption, bucketName, nodeName, restoreFileName, database, restoreType, getInternalIP)
         else:
             if restoreType == 'collection':
                 collection = sys.argv[7]
-                if validateDatabaseCollectionName(database, collection):
-                    restoreMongoCollection(collection, databaseCleanupOption, bucketName, nodeName, restoreFileName, database, restoreType) 
+                if validateDatabaseCollectionName(database, collection, getInternalIP):
+                    restoreMongoCollection(collection, databaseCleanupOption, bucketName, nodeName, restoreFileName, database, restoreType, getInternalIP) 
 
 
 main()
